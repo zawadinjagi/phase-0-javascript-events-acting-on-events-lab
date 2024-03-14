201a77d5837bf3a48d17c9c2b31776617ea5f588
zawadinjagi <zawadinjagi619@gmail.com>
2024-03-14 23:42:38 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const rightBoundary = 360; 
+  
+    if (left < rightBoundary - 40) { 
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keyup", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
